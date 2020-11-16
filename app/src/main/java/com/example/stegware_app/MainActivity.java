49c7dcf56199c7b8753523a6c87aa68f9388c6b3
Daniele Albanese <daniele.albanese98@gmail.com>
2020-11-16 22:59:50 +0100

    Added Encode activity + added cacheService start in MainActivity

--- app/src/main/java/com/example/stegware_app/MainActivity.java
+++ app/src/main/java/com/example/stegware_app/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.stegware_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import com.example.stegware_app.services.CacheService;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    Context chromeContext;
+
+    private static class ResponseReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(CacheService.ACTION)) {
+                Log.d(CacheService.TAG, "Dentro");
+                // TODO
+            }
+        }
+    }
+
+    private final ResponseReceiver receiver = new ResponseReceiver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button encode = findViewById(R.id.encode_button);
+
+        encode.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), Encode.class)));
+
+        Intent cacheService = new Intent(getApplicationContext(), CacheService.class);
+        getApplicationContext().startService(cacheService);
     }
 }
