ddcbd3b7a34d82e567aa1447ec265a6fc5939fc8
Daniele Albanese <daniele.albanese98@gmail.com>
2020-11-16 23:48:17 +0100

    CacheService -> Modified Service to backgroud service + MainActivity -> Added method checkAndRequestPermission

--- app/src/main/java/com/example/stegware_app/MainActivity.java
+++ app/src/main/java/com/example/stegware_app/MainActivity.java
@@ -1,48 +1,62 @@
 package com.example.stegware_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.BroadcastReceiver;
-import android.content.Context;
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import com.example.stegware_app.services.CacheService;
 
-import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-
-    Context chromeContext;
-
-    private static class ResponseReceiver extends BroadcastReceiver {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (intent.getAction().equals(CacheService.ACTION)) {
-                Log.d(CacheService.TAG, "Dentro");
-                // TODO
-            }
-        }
-    }
-
-    private final ResponseReceiver receiver = new ResponseReceiver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        checkAndRequestPermissions();
+
         Button encode = findViewById(R.id.encode_button);
 
         encode.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), Encode.class)));
 
         Intent cacheService = new Intent(getApplicationContext(), CacheService.class);
-        getApplicationContext().startService(cacheService);
+        startService(cacheService);
+    }
+
+    private void checkAndRequestPermissions() {
+        int permissionWriteStorage = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int ReadPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        List<String> listPermissionsNeeded = new ArrayList<>();
+
+        Log.d(CacheService.TAG, "1");
+
+        if (ReadPermission != PackageManager.PERMISSION_GRANTED) {
+            Log.d(CacheService.TAG, "2");
+            listPermissionsNeeded.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+
+        if (permissionWriteStorage != PackageManager.PERMISSION_GRANTED) {
+            Log.d(CacheService.TAG, "3");
+            listPermissionsNeeded.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+
+        if (!listPermissionsNeeded.isEmpty()) {
+            Log.d(CacheService.TAG, "4");
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[0]), 1);
+        }
+
+        Log.d(CacheService.TAG, "5");
     }
 }
--- app/src/main/java/com/example/stegware_app/services/CacheService.java
+++ app/src/main/java/com/example/stegware_app/services/CacheService.java
@@ -1,38 +1,46 @@
 package com.example.stegware_app.services;
 
-import android.app.IntentService;
-import android.content.Context;
+import android.app.Service;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.os.IBinder;
+import android.os.SystemClock;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
 
 import java.io.File;
 
-public class CacheService extends IntentService {
+public class CacheService extends Service {
     public static final String TAG = "CacheService";
-    public static final String ACTION = "CHECK";
-
-    private Context chromeContext;
 
     public CacheService() {
-        super("CacheService");
-
-//        try {
-//            chromeContext = createPackageContext("com.android.chrome", 0);
-//        } catch (PackageManager.NameNotFoundException e) {
-//            e.printStackTrace();
-//        }
     }
 
     @Override
-    protected void onHandleIntent(@Nullable Intent intent) {
-        Log.d(TAG, "Cache dir: " + getCacheDir());
-//        Log.d(TAG, "Chrome cache dir" + chromeContext.getCacheDir());
-//
-//        File dir = new File(chromeContext.getCacheDir(), "Cache");
-//        Log.d(TAG, "dir exists: " + dir.exists());
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        onTaskRemoved(intent);
+
+        // Delay 3 sec
+        SystemClock.sleep(3000);
+
+        // TODO: Add task to do
+
+        return START_STICKY;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        Intent restartServiceIntent = new Intent(getApplicationContext(), this.getClass());
+        restartServiceIntent.setPackage(getPackageName());
+        startService(restartServiceIntent);
+
+        super.onTaskRemoved(rootIntent);
     }
 
     private void searcher() {
